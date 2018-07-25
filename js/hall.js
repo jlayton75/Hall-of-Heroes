0fffd7016893a0613b68e1cc6587483d61baad60
Joshua Curtis Layton <joshlayton75@gmail.com>
2018-07-25 15:27:23 -0500

    Initial commit

---
+++ js/hall.js
@@ -0,0 +1,24 @@
+
+/*
+console.log("test");
+var images=["img/hall1.jpg","img/hall2.jpg","img/hall3.jpg","img/hall4.jpg","img/hall5.jpg",];
+
+
+var i=0;
+
+setInterval(function(){
+
+$('#lander').css("background-image", 'url('+ images[i]+')');  
+
+i++;
+if(i==images.length)
+{
+
+  i=0;
+}
+
+
+},5000);
+
+*/
+
